c7a98a374d752a1e40ee387a09cff9388f0d96d6
Mohamed Gomaa <mo.gomaa.formal@gmail.com>
2024-01-30 18:01:49 +0200

    Add the basic functionality to the tic-tac-toe game

--- script.js
+++ script.js
@@ -1,21 +1,82 @@
-// TODO: Set up your project with HTML, CSS and Javascript files and get the Git repo all set up. => DONE
+const TicTacToe = (function () {
+  let playerTurn = "X";
+  let order = 1;
+  const Gameboard = ["", "", "", "", "", "", "", "", ""];
+  const cellsParent = document.querySelector(".cells");
+  const cells = document.querySelectorAll(".cell");
+  const actualWins = [];
+  // the function checks if the array has any of the following combinations:
+  // [1,2,3]
+  // [4,5,6]
+  // [7,8,9]
+  // [3,5,7]
+  // [1,5,9]
+  // [1,4,7]
+  // [2,5,8]
+  // [3,6,9]
+  const check = function (board) {
+    const winningPositions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8], // horizontal
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8], // vertical
+      [0, 4, 8],
+      [2, 4, 6] // diagonal
+    ];
 
-// TODO: You’re going to store the gameboard as an array inside of a Gameboard object, so start there!
+    for (let i = 0; i < winningPositions.length; i++) {
+      const [a, b, c] = winningPositions[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        actualWins.push(a, b, c);
+        cells[a].classList.add("won");
+        cells[b].classList.add("won");
+        cells[c].classList.add("won");
+        rematchOption();
+        return true;
+      }
+    }
 
-// TODO: Your players are also going to be stored in objects, and you’re probably going to want an object to control the flow of the game itself.
+    return false;
+  };
 
-// TODO: Your main goal here is to have as little global code as possible.
+  const turnInp = (function () {
+    // loop through the cells nodeList and listen for the click event and when the cell is clicked, the innerText of the cell is set to the turn and then the index of the element in the children elements of the .cells parents element is set to its order and then the same index is assigned a value in the Gameboard array and then the winning patterns are checked and then the player is then flipped
+    cells.forEach((El) => {
+      El.addEventListener("click", () => {
+        El.innerText = playerTurn;
+        const index = Array.prototype.indexOf.call(cellsParent.children, El);
+        Gameboard[index] = playerTurn;
+        check(Gameboard);
+        playerTurn = playerTurn === "O" ? "X" : "O";
+      });
+    });
+  })();
+  const rematch = function () {
+    Gameboard.forEach((el) => (Gameboard[Gameboard.indexOf(el)] = ""));
+    playerTurn = "X";
+    cells.forEach((El) => {
+      El.innerText = "";
+    });
+  };
 
-// TODO: Try tucking as much as you can inside factories. If you only need a single instance of something (e.g. the gameboard, the displayController etc.) then wrap the factory inside an IIFE (module pattern) so it cannot be reused to create additional instances.
+  const rematchOption = function () {
+    const rematchBtn = document.createElement("button");
+    rematchBtn.textContent = "Rematch ?";
+    document.querySelector(".tic-tac-toe").appendChild(rematchBtn);
+    rematchBtn.addEventListener("click", () => {
+      rematch();
+      rematchBtn.style.display = "none";
+      cells[actualWins[0]].classList.remove("won");
+      cells[actualWins[1]].classList.remove("won");
+      cells[actualWins[2]].classList.remove("won");
+    });
+  };
 
-// TODO: In this project, think carefully about where each bit of logic should reside. Each little piece of functionality should be able to fit in the game, player or gameboard objects. Take care to put them in “logical” places. Spending a little time brainstorming here can make your life much easier later!
-
-// TODO: If you’re having trouble, Building a house from the inside out is a great article that lays out a highly applicable example both of how you might approach tackling this project as well as how you might organize and structure your code.
-
-// TODO: Focus on getting a working game in the console first. Make sure you include logic that checks for when the game is over! You should be checking for all winning 3-in-a-rows and ties. Try to avoid thinking about the DOM and your HTML/CSS until your game is working.
-
-// TODO: Once you have a working console game, create an object that will handle the display/DOM logic. Write a function that will render the contents of the gameboard array to the webpage (for now, you can always just fill the gameboard array with "X"s and "O"s just to see what’s going on).
-
-// TODO: Write the functions that allow players to add marks to a specific spot on the board by interacting with the appropriate DOM elements (e.g. letting players click on a board square to place their marker). Don’t forget the logic that keeps players from playing in spots that are already taken!
-
-// TODO: Clean up the interface to allow players to put in their names, include a button to start/restart the game and add a display element that shows the results upon game end!
+  return {
+    Gameboard,
+    check,
+    turnInp
+  };
+})();
