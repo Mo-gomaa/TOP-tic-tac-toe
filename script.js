564aea818f678af6cfa3bf0e0e8d527b933d2be0
Mohamed Gomaa <mo.gomaa.formal@gmail.com>
2024-01-30 19:02:45 +0200

    The most basic solution

--- script.js
+++ script.js
@@ -1,82 +1,101 @@
-const TicTacToe = (function () {
-  let playerTurn = "X";
-  let order = 1;
-  const Gameboard = ["", "", "", "", "", "", "", "", ""];
-  const cellsParent = document.querySelector(".cells");
-  const cells = document.querySelectorAll(".cell");
-  const actualWins = [];
-  // the function checks if the array has any of the following combinations:
-  // [1,2,3]
-  // [4,5,6]
-  // [7,8,9]
-  // [3,5,7]
-  // [1,5,9]
-  // [1,4,7]
-  // [2,5,8]
-  // [3,6,9]
-  const check = function (board) {
-    const winningPositions = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8], // horizontal
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8], // vertical
-      [0, 4, 8],
-      [2, 4, 6] // diagonal
-    ];
+const cells = document.querySelectorAll(".cell");
 
-    for (let i = 0; i < winningPositions.length; i++) {
-      const [a, b, c] = winningPositions[i];
-      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-        actualWins.push(a, b, c);
-        cells[a].classList.add("won");
-        cells[b].classList.add("won");
-        cells[c].classList.add("won");
-        rematchOption();
-        return true;
-      }
+const restartBtn = document.querySelector("#rematch");
+
+const winConditions = [
+  [0, 1, 2],
+
+  [3, 4, 5],
+
+  [6, 7, 8],
+
+  [0, 3, 6],
+
+  [1, 4, 7],
+
+  [2, 5, 8],
+
+  [0, 4, 8],
+
+  [2, 4, 6]
+];
+
+let options = ["", "", "", "", "", "", "", "", ""];
+
+let currentPlayer = "X";
+
+let running = false;
+
+initializeGame();
+
+function initializeGame() {
+  cells.forEach((cell) => cell.addEventListener("click", cellClicked));
+
+  restartBtn.addEventListener("click", restartGame);
+
+  running = true;
+}
+
+function cellClicked() {
+  const cellIndex = this.getAttribute("cellIndex");
+
+  if (options[cellIndex] != "" || !running) {
+    return;
+  }
+
+  updateCell(this, cellIndex);
+
+  checkWinner();
+}
+
+function updateCell(cell, index) {
+  options[index] = currentPlayer;
+
+  cell.textContent = currentPlayer;
+}
+
+function changePlayer() {
+  currentPlayer = currentPlayer == "X" ? "O" : "X";
+}
+
+function checkWinner() {
+  let roundWon = false;
+
+  for (let i = 0; i < winConditions.length; i++) {
+    const condition = winConditions[i];
+
+    const cellA = options[condition[0]];
+
+    const cellB = options[condition[1]];
+
+    const cellC = options[condition[2]];
+
+    if (cellA == "" || cellB == "" || cellC == "") {
+      continue;
     }
 
-    return false;
-  };
+    if (cellA == cellB && cellB == cellC) {
+      roundWon = true;
 
-  const turnInp = (function () {
-    // loop through the cells nodeList and listen for the click event and when the cell is clicked, the innerText of the cell is set to the turn and then the index of the element in the children elements of the .cells parents element is set to its order and then the same index is assigned a value in the Gameboard array and then the winning patterns are checked and then the player is then flipped
-    cells.forEach((El) => {
-      El.addEventListener("click", () => {
-        El.innerText = playerTurn;
-        const index = Array.prototype.indexOf.call(cellsParent.children, El);
-        Gameboard[index] = playerTurn;
-        check(Gameboard);
-        playerTurn = playerTurn === "O" ? "X" : "O";
-      });
-    });
-  })();
-  const rematch = function () {
-    Gameboard.forEach((el) => (Gameboard[Gameboard.indexOf(el)] = ""));
-    playerTurn = "X";
-    cells.forEach((El) => {
-      El.innerText = "";
-    });
-  };
+      break;
+    }
+  }
 
-  const rematchOption = function () {
-    const rematchBtn = document.createElement("button");
-    rematchBtn.textContent = "Rematch ?";
-    document.querySelector(".tic-tac-toe").appendChild(rematchBtn);
-    rematchBtn.addEventListener("click", () => {
-      rematch();
-      rematchBtn.style.display = "none";
-      cells[actualWins[0]].classList.remove("won");
-      cells[actualWins[1]].classList.remove("won");
-      cells[actualWins[2]].classList.remove("won");
-    });
-  };
+  if (roundWon) {
+    running = false;
+  } else if (!options.includes("")) {
+    running = false;
+  } else {
+    changePlayer();
+  }
+}
 
-  return {
-    Gameboard,
-    check,
-    turnInp
-  };
-})();
+function restartGame() {
+  currentPlayer = "X";
+
+  options = ["", "", "", "", "", "", "", "", ""];
+
+  cells.forEach((cell) => (cell.textContent = ""));
+
+  running = true;
+}
